250da877b954b9198ba4769a2274a7c1ab6c4f59
Lizzie <jonesmlizzie@gmail.com>
2024-11-26 11:41:39 -0500

    added routing

--- server/controller/community.ts
+++ server/controller/community.ts
@@ -118,25 +118,6 @@ const communityController = (socket: FakeSOSocket) => {
     }
   };
 
-  const getCommunityMembers = async (req: Request, res: Response): Promise<void> => {
-    const { community } = req.params;
-    console.log(`Received request to get members of community: ${community}`);
-
-    try {
-      const communityData = await CommunityModel.findOne({ name: community });
-      if (!communityData) {
-        console.log(`Community not found: ${community}`);
-        res.status(404).json({ message: 'Community not found' });
-        return;
-      }
-      console.log(`Retrieved users for community ${community}: ${communityData.users}`);
-      res.status(200).json(communityData.users);
-    } catch (error) {
-      console.error('Error retrieving users for the community:', error);
-      res.status(500).json({ error: 'Error retrieving questions for the community' });
-    }
-  };
-
   const getRelevantCommunities = async (req: Request, res: Response): Promise<void> => {
     const { tags } = req.query;
 
@@ -195,6 +176,26 @@ const communityController = (socket: FakeSOSocket) => {
       res.status(500).json({ error: 'Error updating community questions.' });
     }
   };
+
+  const getCommunityMembers = async (req: Request, res: Response): Promise<void> => {
+    const { community } = req.params;
+    console.log(`Received request to get members of community: ${community}`);
+
+    try {
+      const communityData = await CommunityModel.findOne({ name: community });
+      if (!communityData) {
+        console.log(`Community not found: ${community}`);
+        res.status(404).json({ message: 'Community not found' });
+        return;
+      }
+      console.log(`Retrieved users for community ${community}: ${communityData.users}`);
+      res.status(200).json(communityData.users);
+    } catch (error) {
+      console.error('Error retrieving users for the community:', error);
+      res.status(500).json({ error: 'Error retrieving questions for the community' });
+    }
+  };
+
   router.get('/getCommunityNames', getCommunityNames);
   router.get('/getCommunityByName/:name', getCommunityByName);
   router.get('/getCommunityQuestions/:community', getCommunityQuestions);
@@ -202,6 +203,7 @@ const communityController = (socket: FakeSOSocket) => {
   router.get('/getRelevantCommunities', getRelevantCommunities);
   router.patch('/addUserToCommunity/:communityName', addUserToCommunity);
   router.patch('/updateCommunityQuestions/:communityName', updateCommunityQuestions);
+  router.get('/getCommunityMembers/:community', getCommunityMembers);
 
   return router;
 };
